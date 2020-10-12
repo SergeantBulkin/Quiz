b7b7a4bff2fb342d058b8a432d51f9a6be8525e4
SergeantBulkin <bezdeneg.ya@yandex.ru>
2020-10-12 12:56:20 +0300

    Added CategoryDialogBottomSheet to choose category

---
+++ app/src/main/java/by/bsuir/kulinka/quiz/fragment/CategoryListDialogFragment.java
@@ -0,0 +1,137 @@
+package by.bsuir.kulinka.quiz.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import by.bsuir.kulinka.quiz.R;
+
+/**
+ * <p>A fragment that shows a list of items as a modal bottom sheet.</p>
+ * <p>You can show this modal bottom sheet from your activity like this:</p>
+ * <pre>
+ *     CategoryListDialogFragment.newInstance(30).show(getSupportFragmentManager(), "dialog");
+ * </pre>
+ */
+public class CategoryListDialogFragment extends BottomSheetDialogFragment
+{
+    private CategoryDialogItemListener listener;
+    //----------------------------------------------------------------------------------------------
+    private static final String ARG_CATEGORIES = "categories_list";
+    //----------------------------------------------------------------------------------------------
+    public static CategoryListDialogFragment newInstance(ArrayList<String> categoriesList, CategoryDialogItemListener listener)
+    {
+        final CategoryListDialogFragment fragment = new CategoryListDialogFragment();
+        fragment.setListener(listener);
+        final Bundle args = new Bundle();
+        args.putStringArrayList(ARG_CATEGORIES, categoriesList);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void setListener(CategoryDialogItemListener listener)
+    {
+        this.listener = listener;
+    }
+
+    //----------------------------------------------------------------------------------------------
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_dialog_category, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        final RecyclerView recyclerView = (RecyclerView) view;
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        if (getArguments() != null)
+        {
+            recyclerView.setAdapter(new CategoryAdapter(getArguments().getStringArrayList(ARG_CATEGORIES), listener));
+        }
+    }
+    //----------------------------------------------------------------------------------------------
+    //Адаптер выбора категории
+    private class CategoryAdapter extends RecyclerView.Adapter<ViewHolder>
+    {
+        //------------------------------------------------------------------------------------------
+        private List<String> categories;
+        CategoryDialogItemListener listener;
+        //------------------------------------------------------------------------------------------
+        CategoryAdapter(List<String> categoriesList, CategoryDialogItemListener listener)
+        {
+            this.categories = categoriesList;
+            this.listener = listener;
+        }
+        //------------------------------------------------------------------------------------------
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+        {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+        //------------------------------------------------------------------------------------------
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position)
+        {
+            holder.text.setText(categories.get(position));
+            holder.setListener(position);
+        }
+        //------------------------------------------------------------------------------------------
+        @Override
+        public int getItemCount()
+        {
+            if (categories == null)
+            {
+                return 0;
+            }
+            return categories.size();
+        }
+        //------------------------------------------------------------------------------------------
+    }
+    //----------------------------------------------------------------------------------------------
+    private class ViewHolder extends RecyclerView.ViewHolder
+    {
+        final TextView text;
+        //------------------------------------------------------------------------------------------
+        ViewHolder(LayoutInflater inflater, ViewGroup parent)
+        {
+            super(inflater.inflate(R.layout.fragment_dialog_category_item, parent, false));
+            text = itemView.findViewById(R.id.category_name);
+        }
+        //------------------------------------------------------------------------------------------
+        //Установить слушателя
+        private void setListener(int position)
+        {
+            itemView.setOnClickListener(v ->
+            {
+                listener.categorySelected(position);
+                requireDialog().dismiss();
+            });
+        }
+        //------------------------------------------------------------------------------------------
+    }
+    //----------------------------------------------------------------------------------------------
+    //Интерфейс для выбора категории
+    public interface CategoryDialogItemListener
+    {
+        void categorySelected(int index);
+    }
+    //----------------------------------------------------------------------------------------------
+}
--- app/src/main/java/by/bsuir/kulinka/quiz/fragment/MainMenuFragment.java
+++ app/src/main/java/by/bsuir/kulinka/quiz/fragment/MainMenuFragment.java
@@ -5,25 +5,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import by.bsuir.kulinka.quiz.databinding.FragmentMainMenuBinding;
 
-public class MainMenuFragment extends Fragment
+public class MainMenuFragment extends Fragment implements CategoryListDialogFragment.CategoryDialogItemListener
 {
+    //----------------------------------------------------------------------------------------------
     FragmentMainMenuBinding binding;
-
+    //Список категорий
+    ArrayList<String> categories;
+    //----------------------------------------------------------------------------------------------
     public MainMenuFragment()
     {
         // Required empty public constructor
     }
-
+    //----------------------------------------------------------------------------------------------
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
     }
-
+    //----------------------------------------------------------------------------------------------
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
@@ -31,4 +37,58 @@ public class MainMenuFragment extends Fragment
         binding = FragmentMainMenuBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
+    //----------------------------------------------------------------------------------------------
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Init
+        init();
+
+        //Настройка кнопок
+        setUpButtons();
+    }
+    //----------------------------------------------------------------------------------------------
+    private void init()
+    {
+        categories = new ArrayList<>();
+        categories.add("Машины");
+        categories.add("История");
+        categories.add("Искусство");
+        categories.add("Технологии");
+        categories.add("Наука");
+    }
+    //----------------------------------------------------------------------------------------------
+    //Настройка кнопок
+    private void setUpButtons()
+    {
+        //Слушатель на кнопку "Играть"
+        binding.mainMenuPlayButton.setOnClickListener(v ->
+        {
+
+        });
+
+        //Слушатель на кнопку "Выход"
+        binding.mainMenuExitButton.setOnClickListener(v ->
+        {
+            requireActivity().finish();
+        });
+
+        //Слушатель на кнопку выбора категории
+        binding.mainMenuCategoryButton.setOnClickListener(v ->
+        {
+            //Показать диалог выбора категории
+            CategoryListDialogFragment.newInstance(categories, this).show(requireFragmentManager(), "dialog");
+        });
+    }
+    //----------------------------------------------------------------------------------------------
+    //Метод интерфейса CategoryDialogItemListener
+    @Override
+    public void categorySelected(int index)
+    {
+        binding.mainMenuCategoryButton.setText(categories.get(index));
+    }
+    //----------------------------------------------------------------------------------------------
 }
